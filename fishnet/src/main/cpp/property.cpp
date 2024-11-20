5afa7ff2d2e720ed7ccf7fe87e5f723d0bfe0ea8
Kyant0 <kyant2021@outlook.com>
2024-11-20 12:45:26 +0800

    Reimplement get_page_size

--- fishnet/src/main/cpp/property.cpp
+++ fishnet/src/main/cpp/property.cpp
@@ -1,6 +1,5 @@
 #include "property.h"
 
-#include <bits/sysconf.h>
 #include <sys/system_properties.h>
 
 std::string get_property(const char *key, const char *default_value) {
@@ -13,12 +12,4 @@ std::string get_property(const char *key, const char *default_value) {
 
 bool get_bool_property(const char *key, bool default_value) {
     return strcmp(get_property(key, default_value ? "1" : "0").c_str(), "1") == 0;
-}
-
-size_t get_page_size() {
-#if defined(PAGE_SIZE)
-    return PAGE_SIZE;
-#else
-    return sysconf(_SC_PAGESIZE);
-#endif
 }
--- fishnet/src/main/cpp/property.h
+++ fishnet/src/main/cpp/property.h
@@ -2,11 +2,18 @@
 #define FISHNET_PROPERTY_H
 
 #include <string>
+#include <sys/auxv.h>
 
 std::string get_property(const char *key, const char *default_value);
 
 bool get_bool_property(const char *key, bool default_value);
 
-size_t get_page_size();
+inline size_t get_page_size() {
+#if defined(PAGE_SIZE)
+    return PAGE_SIZE;
+#else
+    return (size_t) getauxval(AT_PAGESZ);
+#endif
+}
 
 #endif //FISHNET_PROPERTY_H
