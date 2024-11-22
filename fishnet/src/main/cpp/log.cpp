e1c451b1e7ebe5f3f962d6a08a50f4d92bdbb114
Kyant0 <kyant2021@outlook.com>
2024-11-22 17:26:19 +0800

    Disable log print

--- fishnet/src/main/cpp/log.cpp
+++ fishnet/src/main/cpp/log.cpp
@@ -80,10 +80,10 @@ void StringAppendF(std::string *dst, const char *format, ...) {
 void log_fishnet(bool linebreak, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
-    size_t next_start = log_buffer.size();
+    // size_t next_start = log_buffer.size();
     StringAppendV(&log_buffer, strdup(fmt), args);
     va_end(args);
-    __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "%s", log_buffer.c_str() + next_start);
+    // __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "%s", log_buffer.c_str() + next_start);
     if (linebreak) {
         log_buffer.append("\n");
     }
