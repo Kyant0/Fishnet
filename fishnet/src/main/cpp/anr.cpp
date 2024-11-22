879c8a15f1ba280d68879deb9aef8362a761131e
Kyant0 <kyant2021@outlook.com>
2024-11-22 20:29:55 +0800

    Refactor ANR buffers

--- fishnet/src/main/cpp/anr.cpp
+++ fishnet/src/main/cpp/anr.cpp
@@ -25,8 +25,8 @@ static bool is_number_str(const char *str, size_t max_len) {
 }
 
 int get_signal_catcher_tid(pid_t myPid) {
-    char *processPath = new char[MAX_BUFFER_SIZE];
-    size_t size = sprintf(processPath, "/proc/%d/task", myPid);
+    char processPath[MAX_BUFFER_SIZE];
+    size_t size = snprintf(processPath, sizeof(processPath), "/proc/%d/task", myPid);
     if (size >= MAX_BUFFER_SIZE) {
         LOG_FISHNET("Read proc path fail, read buffer size: %zu", size);
         return -1;
@@ -34,33 +34,29 @@ int get_signal_catcher_tid(pid_t myPid) {
     DIR *processDir = opendir(processPath);
     if (processDir) {
         int tid = -1;
+        char filePath[MAX_BUFFER_SIZE];
+        char threadName[MAX_BUFFER_SIZE];
         dirent *child = readdir(processDir);
         while (child != nullptr) {
             if (is_number_str(child->d_name, MAX_BUFFER_SIZE - 1)) {
-                char *filePath = new char[MAX_BUFFER_SIZE];
-                size = sprintf(filePath, "%s/%s/comm", processPath, child->d_name);
+                size = snprintf(filePath, sizeof(filePath), "%s/%s/comm", processPath, child->d_name);
                 if (size >= MAX_BUFFER_SIZE) {
                     continue;
                 }
-                char *threadName = new char[MAX_BUFFER_SIZE];
                 int fd = open(filePath, O_RDONLY);
-                delete[] filePath;
                 size = read(fd, threadName, MAX_BUFFER_SIZE);
                 close(fd);
                 threadName[size - 1] = '\0';
                 if (strcmp(threadName, "Signal Catcher") == 0) {
                     tid = atoi(child->d_name);
-                    delete[] threadName;
                     break;
                 }
             }
             child = readdir(processDir);
         }
         closedir(processDir);
-        delete[] processPath;
         return tid;
     } else {
-        delete[] processPath;
         LOG_FISHNET("Read process dir fail, error: %s", strerror(errno));
     }
     return -1;
