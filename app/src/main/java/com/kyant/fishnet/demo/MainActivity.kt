4992901946a308cf37f5d0551119cd0ff01bba58
Kyant0 <kyant2021@outlook.com>
2024-11-27 15:29:30 +0800

    Use TabHost

--- app/src/main/java/com/kyant/fishnet/demo/MainActivity.kt
+++ app/src/main/java/com/kyant/fishnet/demo/MainActivity.kt
@@ -1,94 +1,51 @@
+@file:Suppress("DEPRECATION")
+
 package com.kyant.fishnet.demo
 
 import android.app.Activity
-import android.graphics.Outline
-import android.graphics.drawable.Drawable
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.ViewOutlineProvider
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.TabHost
 
 class MainActivity : Activity() {
-    private lateinit var selectedTabDrawable: Drawable
-    private var selectedTabColor: Int = 0
-    private var unselectedTabColor: Int = 0
-
-    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tabHost = findViewById<TabHost>(R.id.tab_host)
+        tabHost.setup()
+
+        tabHost.addTab(
+            tabHost
+                .newTabSpec("crash")
+                .setIndicator("Crash")
+                .setContent(android.R.id.tabcontent)
+        )
+        tabHost.addTab(
+            tabHost
+                .newTabSpec("log")
+                .setIndicator("Log")
+                .setContent(android.R.id.tabcontent)
+        )
+        tabHost.addTab(
+            tabHost
+                .newTabSpec("disasm")
+                .setIndicator("DisAsm")
+                .setContent(android.R.id.tabcontent)
+        )
+
         fragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, CrashFragment())
+            .replace(R.id.tab_content, CrashFragment())
             .commit()
 
-        val tabLayout = findViewById<LinearLayout>(R.id.tab_layout)
-        val crashTab = findViewById<TextView>(R.id.tab_crash)
-        val logTab = findViewById<TextView>(R.id.tab_log)
-        val disasmTab = findViewById<TextView>(R.id.tab_disasm)
+        tabHost.setOnTabChangedListener { tabId ->
+            val transaction = fragmentManager.beginTransaction()
 
-        selectedTabDrawable = resources.getDrawable(R.color.tab, theme)
-        selectedTabColor = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            resources.getColor(R.color.tab_selected_text, theme)
-        } else {
-            resources.getColor(R.color.tab_selected_text)
-        }
-        unselectedTabColor = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            resources.getColor(R.color.text, theme)
-        } else {
-            resources.getColor(R.color.text)
-        }
-
-        val cornerRadiusPx = 12 * resources.displayMetrics.density
-        tabLayout.outlineProvider = object : ViewOutlineProvider() {
-            override fun getOutline(view: View, outline: Outline) {
-                outline.setRoundRect(0, 0, view.width, view.height, cornerRadiusPx)
+            when (tabId) {
+                "crash" -> transaction.replace(R.id.tab_content, CrashFragment())
+                "log" -> transaction.replace(R.id.tab_content, LogFragment())
+                "disasm" -> transaction.replace(R.id.tab_content, DisAsmFragment())
             }
+            transaction.commit()
         }
-        tabLayout.clipToOutline = true
-
-        crashTab.select()
-        logTab.unselect()
-        disasmTab.unselect()
-
-        crashTab.setOnClickListener {
-            crashTab.select()
-            logTab.unselect()
-            disasmTab.unselect()
-
-            fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, CrashFragment())
-                .commit()
-        }
-        logTab.setOnClickListener {
-            crashTab.unselect()
-            logTab.select()
-            disasmTab.unselect()
-
-            fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, LogFragment())
-                .commit()
-        }
-        disasmTab.setOnClickListener {
-            crashTab.unselect()
-            logTab.unselect()
-            disasmTab.select()
-
-            fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, DisAsmFragment())
-                .commit()
-        }
-    }
-
-    private fun TextView.select() {
-        setTextColor(selectedTabColor)
-        background = selectedTabDrawable
-    }
-
-    private fun TextView.unselect() {
-        setTextColor(unselectedTabColor)
-        background = null
     }
 }
