37a5ea47c66e0201eee0b2eb6081a095e2b370ce
Kyant0 <kyant2021@outlook.com>
2024-11-28 13:01:55 +0800

    Fix crash on older API

--- fishnet/src/main/java/com/kyant/fishnet/Fishnet.kt
+++ fishnet/src/main/java/com/kyant/fishnet/Fishnet.kt
@@ -2,22 +2,35 @@ package com.kyant.fishnet
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 
 object Fishnet {
+    @Suppress("DEPRECATION")
     fun init(context: Context, fd: Int): Boolean {
         val packageName = context.packageName
-        val packageInfo = context.packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+        val packageInfo =
+            context.packageManager.getPackageInfo(
+                packageName,
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) PackageManager.GET_SIGNING_CERTIFICATES
+                else PackageManager.GET_SIGNATURES
+            )
         val versionName = packageInfo.versionName ?: "???"
-        val versionCode = packageInfo.longVersionCode
-        val signingInfo = packageInfo.signingInfo
-        val cert = if (signingInfo != null) {
-            if (!signingInfo.hasMultipleSigners()) {
-                signingInfo.signingCertificateHistory?.getOrNull(0)?.toChars()?.take(8)?.joinToString("")
+        val versionCode =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) packageInfo.longVersionCode
+            else packageInfo.versionCode.toLong()
+        val cert = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val signingInfo = packageInfo.signingInfo
+            if (signingInfo != null) {
+                if (!signingInfo.hasMultipleSigners()) {
+                    signingInfo.signingCertificateHistory?.getOrNull(0)?.toChars()?.take(8)?.joinToString("")
+                } else {
+                    signingInfo.apkContentsSigners?.joinToString { it.toChars().take(8).joinToString("") }
+                } ?: "???"
             } else {
-                signingInfo.apkContentsSigners?.joinToString { it.toChars().take(8).joinToString("") }
-            } ?: "???"
+                "???"
+            }
         } else {
-            "???"
+            packageInfo.signatures?.joinToString { it.toChars().take(8).joinToString("") } ?: "???"
         }
         return nativeInit(fd, packageName, versionName, versionCode, cert)
     }
