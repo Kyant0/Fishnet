6d6ef3717f8ba22f0c11cdc4e766889df2944f1a
Kyant0 <kyant2021@outlook.com>
2024-11-30 14:15:41 +0800

    Use Theme.Fishnet.Base, use actionbar tabs

--- app/src/main/java/com/kyant/fishnet/demo/MainActivity.kt
+++ app/src/main/java/com/kyant/fishnet/demo/MainActivity.kt
@@ -1,62 +1,52 @@
-@file:Suppress("DEPRECATION")
+@file:Suppress("DEPRECATION", "OVERRIDE_DEPRECATION")
 
 package com.kyant.fishnet.demo
 
+import android.app.ActionBar
 import android.app.Activity
+import android.app.Fragment
+import android.app.FragmentTransaction
 import android.os.Bundle
-import android.widget.TabHost
-import android.widget.TabWidget
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tabHost = findViewById<TabHost>(R.id.tab_host)
-        tabHost.setup()
-
-        tabHost.addTab(
-            tabHost
-                .newTabSpec("crash")
-                .setIndicator("Crash")
-                .setContent(android.R.id.tabcontent)
-        )
-        tabHost.addTab(
-            tabHost
-                .newTabSpec("log")
-                .setIndicator("Log")
-                .setContent(android.R.id.tabcontent)
-        )
-        tabHost.addTab(
-            tabHost
-                .newTabSpec("disasm")
-                .setIndicator("DisAsm")
-                .setContent(android.R.id.tabcontent)
-        )
-
-        fragmentManager.beginTransaction()
-            .replace(R.id.tab_content, CrashFragment())
-            .commit()
-
-        findViewById<TabWidget>(android.R.id.tabs).apply {
-            getChildAt(0).setOnClickListener {
-                tabHost.currentTab = 0
-                fragmentManager.beginTransaction()
-                    .replace(R.id.tab_content, CrashFragment())
-                    .commit()
+        checkNotNull(actionBar).apply {
+            navigationMode = ActionBar.NAVIGATION_MODE_TABS
+            newTab().apply {
+                text = "Crash"
+                setTabListener(TabListener(CrashFragment()))
+                addTab(this)
             }
-            getChildAt(1).setOnClickListener {
-                tabHost.currentTab = 1
-                fragmentManager.beginTransaction()
-                    .replace(R.id.tab_content, LogFragment())
-                    .commit()
+            newTab().apply {
+                text = "Log"
+                setTabListener(TabListener(LogFragment()))
+                addTab(this)
             }
-            getChildAt(2).setOnClickListener {
-                tabHost.currentTab = 2
-                fragmentManager.beginTransaction()
-                    .replace(R.id.tab_content, DisAsmFragment())
-                    .commit()
+            newTab().apply {
+                text = "DisAsm"
+                setTabListener(TabListener(DisAsmFragment()))
+                addTab(this)
             }
+        }
+    }
+
+    private inner class TabListener(private val fragment: Fragment) : ActionBar.TabListener {
+        override fun onTabSelected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
+            fragmentManager.beginTransaction()
+                .replace(R.id.content, fragment)
+                .commit()
+        }
+
+        override fun onTabUnselected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
+        }
+
+        override fun onTabReselected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
+            fragmentManager.beginTransaction()
+                .replace(R.id.content, fragment)
+                .commit()
         }
     }
 }
