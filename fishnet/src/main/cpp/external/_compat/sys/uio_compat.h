7123ff7ae5c3c1160f18df09693331a836ad6141
Kyant0 <kyant2021@outlook.com>
2024-11-30 13:38:00 +0800

    Simplify code

--- fishnet/src/main/cpp/external/_compat/sys/uio_compat.h
+++ fishnet/src/main/cpp/external/_compat/sys/uio_compat.h
@@ -7,52 +7,51 @@
 #include <unistd.h>
 
 static ssize_t process_vm_readv_compat(pid_t pid,
-                                         const struct iovec *local_iov, unsigned long liovcnt,
-                                         const struct iovec *remote_iov, unsigned long riovcnt,
-                                         unsigned long flags) {
+                                       const struct iovec *local_iov, unsigned long liovcnt,
+                                       const struct iovec *remote_iov, unsigned long riovcnt,
+                                       unsigned long flags) {
     if (__builtin_available(android 23, *)) {
         return process_vm_readv(pid, local_iov, liovcnt, remote_iov, riovcnt, flags);
-    } else {
-        char mem_path[18];
-        snprintf(mem_path, sizeof(mem_path), "/proc/%d/mem", pid);
+    }
 
-        const int mem_fd = open(mem_path, O_RDONLY);
-        if (mem_fd < 0) {
-            return -1;
-        }
+    char mem_path[18];
+    snprintf(mem_path, sizeof(mem_path), "/proc/%d/mem", pid);
 
-        ssize_t total_read = 0;
+    const int mem_fd = open(mem_path, O_RDONLY);
+    if (mem_fd < 0) {
+        return -1;
+    }
 
-        for (int i = 0; i < riovcnt; ++i) {
-            const struct iovec *remote = &remote_iov[i];
-            void *remote_addr = remote->iov_base;
-            size_t remote_len = remote->iov_len;
+    ssize_t total_read = 0;
 
-            for (int j = 0; j < liovcnt; ++j) {
-                const struct iovec *local = &local_iov[j];
-                void *local_addr = local->iov_base;
-                const size_t local_len = local->iov_len;
+    for (size_t i = 0; i < riovcnt; ++i) {
+        const struct iovec *remote = &remote_iov[i];
+        void *remote_addr = remote->iov_base;
+        size_t remote_len = remote->iov_len;
 
-                const size_t to_read = (remote_len < local_len) ? remote_len : local_len;
-                const ssize_t bytes_read =
-                        pread(mem_fd, local_addr, to_read, (off_t) remote_addr);
-                if (bytes_read < 0) {
-                    close(mem_fd);
-                    return -1;
-                }
+        for (size_t j = 0; j < liovcnt; ++j) {
+            const struct iovec *local = &local_iov[j];
+            void *local_addr = local->iov_base;
+            const size_t local_len = local->iov_len;
 
-                total_read += bytes_read;
-                remote_addr = (char *) remote_addr + bytes_read;
-                remote_len -= bytes_read;
-                if (remote_len == 0) {
-                    break;
-                }
+            const size_t to_read = (remote_len < local_len) ? remote_len : local_len;
+            const ssize_t bytes_read = pread(mem_fd, local_addr, to_read, (off_t) remote_addr);
+            if (bytes_read < 0) {
+                close(mem_fd);
+                return -1;
+            }
+
+            total_read += bytes_read;
+            remote_addr = (char *) remote_addr + bytes_read;
+            remote_len -= bytes_read;
+            if (remote_len == 0) {
+                break;
             }
         }
-
-        close(mem_fd);
-        return total_read;
     }
+
+    close(mem_fd);
+    return total_read;
 }
 
 #endif //UIO_COMPAT_H
