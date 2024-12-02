e090f65048df06dfdce26bfd259549e9501267c9
Kyant0 <kyant2021@outlook.com>
2024-12-02 21:39:01 +0800

    Use JvmStatic for Fishnet.init

--- fishnet/src/main/java/com/kyant/fishnet/Fishnet.kt
+++ fishnet/src/main/java/com/kyant/fishnet/Fishnet.kt
@@ -3,6 +3,7 @@ package com.kyant.fishnet
 import android.content.Context
 
 object Fishnet {
+    @JvmStatic
     fun init(context: Context, path: String) {
         NativeSignalHandler.init(context, path)
         JavaExceptionHandler.init()
